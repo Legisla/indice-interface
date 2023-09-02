e77007da57b94ce088f0535337366e4c9ee42e44
Pedro Markun <pedro@markun.com.br>
2023-09-02 12:03:18 -0300

    external import pra python

---
+++ app/Services/ExternalImportService.php
@@ -0,0 +1,35 @@
+<?php 
+namespace App\Services;
+
+use App\Models\CongresspersonIndicator;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+class ExternalImportService implements ToModel, WithHeadingRow
+{
+    public function model(array $row)
+    {
+        $congresspersonName = $row['Deputado'];
+        $congressperson = Congressperson::firstOrCreate(['name' => $congresspersonName]);
+
+        for ($i = 1; $i <= 16; $i++) {
+            $value = $row["variavel_{$i}"];
+            $score = $row["variavel_{$i}_score"];
+            $stars = $row["variavel_{$i}_stars"];
+
+            CongresspersonIndicator::updateOrCreate(
+                [
+                    'fk_indicator_id' => $i,
+                    'fk_congressperson_id' => $congressperson->id,
+                ],
+                [
+                    'value' => $value,
+                    'score' => $score,
+                    'outlier' => 0, #TODO: Consertar depois.
+                ]
+            );
+        }
+    }
+}
+
+?>
